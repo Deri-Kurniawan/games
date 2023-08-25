2dcbf177839e2f2c0bb2717c1383170d5d259219
Deri-Kurniawan <deri.netuchi@gmail.com>
2023-08-25 20:42:58 +0700

    feat(lib): add nextui@2.1.5

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import { type Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
 } satisfies Config;
